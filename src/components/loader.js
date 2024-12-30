54a286551e75a9317b7e976316ec6de92cb23d0b
Abdul Mohiz <skylark7768@gmail.com>
2024-12-30 13:53:20 +0500

    completed initial loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -11,17 +11,16 @@ const Loader = () => {
 
     // Animate the text appearance
 
-    tl.to("#loader", {
+    tl.to(".line", {
       opacity: 1,
-
+      duration: 0.1
     })
     tl.from(".line h1, .line h2", {
       y: 150,
-      stagger: 0.5,
+      stagger: 0.2,
       duration: 0.6,
       delay: 0.5,
     });
-
     // Increment the number and show opacity
     tl.from(".line1-part1, .line h2", {
       opacity: 0,
@@ -35,7 +34,6 @@ const Loader = () => {
         }, 33); // Adjust this interval as needed for timing
       },
     });
-   
     tl.to(".line h1", {
       opacity: 0,
       delay: 3.2,
@@ -46,30 +44,32 @@ const Loader = () => {
       delay: 0.1, // Optional delay after the count reaches 100
       y: "-100%", // Move loader out of view upwards
       duration: 1, // Duration of the upward slide
-      ease: "power1.inOut",
+      ease: "power3.inOut",
     });
- 
     tl.set("#loader", { display: "none" });
 
-  }, []);
+    
+  },
+  
+  []);
 
   return (
     <>
       <div
         id="loader"
-        className="opacity-0 h-[100vh] w-full fixed z-[999] text-white bg-black lg:px-[25vh] md:px-[20px] sm:px-[15px] sm:py-[70vw] md:py-[50vw]  lg:py-[10vw] leading-tight"
+        className=" h-[100vh] w-full fixed z-[999] text-white bg-black lg:px-[25vh] md:px-[20px] sm:px-[15px] sm:py-[60vw] md:py-[50vw]  lg:py-[10vw] leading-tight"
       >
-        <div className="line ">
-          <div className="line1-part1">
+        <div className="line opacity-0 md:flex-row sm:flex-col sm:items-start ">
+          <div className="line1-part1 md:pt-[14px] ">
             <h5 className="font-silkSerif ">00</h5>
             <h6 className="font-silkSerif">- 100</h6>
           </div>
           <h1>Your</h1>
         </div>
-        <div className="line">
+        <div className="line opacity-0">
           <h1>Web Experience</h1>
         </div>
-        <div className="line">
+        <div className="line opacity-0">
           <h1>Is Loading Right</h1>
           <h2>Now</h2>
         </div>
